bcf4118b7e562e717e66256f9781706ba8553534
diegoRojasgarcia <diego.rojas02@alumnos.ucn.cl>
2024-08-15 02:25:11 -0400

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b8288f31459a97d"; }
+/******/ 		__webpack_require__.h = function() { return "3bad233292a09aa1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import { ENV } from "@/utils";
 export class Auth {
   async register(data) {
     try {
-      const url = "{ENV.API_URL}/{ENV.ENDPOINTS.AUTH.REGISTER}";
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.AUTH.REGISTER}`;
       const params = {
         method: "POST",
         headers: {
